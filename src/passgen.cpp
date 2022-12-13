d3d889c320e1e2a14c353d7c51062970ee120939
am <am2022a2@gmail.com>
2022-12-13 09:39:40 +0330

    get number of characters and characters and calculate length of the characters

--- src/passgen.cpp
+++ src/passgen.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-usign namespace std;
+using namespace std;
 
 int main(){
-    
+    int num;
+    cout<<"enter number:";
+    cin>>num;
+
+    char ch[num];
+    cout<<"enter characters:";
+    cin>>ch;
+
+    int len = sizeof(ch) / sizeof(ch[0]);
+
+    return 0;
 }
