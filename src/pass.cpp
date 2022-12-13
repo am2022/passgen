0989d369de527e618888c5f0052766fed0500b9e
am <am2022a2@gmail.com>
2022-12-13 11:12:36 +0330

    fix a bug

--- src/pass.cpp
+++ src/pass.cpp
@@ -17,6 +17,7 @@ void pass::creat_pass(char *ch, int len){
 void pass::generate(char *ch, int i, string s, int len){
     if(i == 0){
         cout<<s<<"\n";
+        return;
     }
 
     for(int j = 0;j < len;j++){
