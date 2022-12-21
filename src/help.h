a7e3b12265fe9ece4f91aedb5ad1e7131de1bf8b
am <am2022a2@gmail.com>
2022-12-21 12:55:56 +0330

    update help.h file

--- src/help.h
+++ src/help.h
@@ -3,5 +3,7 @@ using namespace std;
 
 void argv_help(){
     cout<<"-h or --help for getting help\n";
-    cout<<"-char for enter the characters";
+    cout<<"-char for enter the characters\n";
+    cout<<"-o for get output file\n";
+    cout<<"you should type -exec end of your inputs";
 }
