50415b3edb8c509495ec97a5ad0c749a3b98501e
am <am2022a2@gmail.com>
2023-01-06 10:20:36 +0330

    write help for -sr in help.h

--- src/help.h
+++ src/help.h
@@ -5,5 +5,6 @@ void argv_help(){
     cout<<"-h or --help for getting help\n";
     cout<<"-char for enter the characters\n";
     cout<<"-o for get output file\n";
+    cout<<"-sr for start range of password list\n";
     cout<<"you should type -exec end of your inputs";
 }
