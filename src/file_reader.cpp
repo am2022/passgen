71c644ba0410aae1c14b667de9831864a8ff0fb4
am <am2022a2@gmail.com>
2023-01-11 09:35:17 +0330

    edit read_file function

--- src/file_reader.cpp
+++ src/file_reader.cpp
@@ -14,5 +14,18 @@ void file_reader::set_filename(string fname){
 }
 
 string file_reader::read_file(){
-    
+    char buff[MAX_READ_SIZE];
+    int i = 0;
+    string s_buff;
+
+    while(! this->char_in.eof()){
+        this->char_in>>buff[i];
+        i++;
+    }
+
+    for(int j = 0;buff[j] != '\0';i++){
+        s_buff += buff[j];
+    }
+
+    return s_buff;
 }
