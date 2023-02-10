3aed8be8d316f3097d65b1cc9f4440d079f62b5a
am <am2022a2@gmail.com>
2023-02-10 18:27:05 +0330

    update help.h file

--- src/help.h
+++ src/help.h
@@ -5,6 +5,7 @@ void argv_help(){
     cout<<"-h or --help for getting help\n";
     cout<<"-char for enter the characters\n";
     cout<<"\t\tyou can use --num to set 0123456789 to chars\n";
+    cout<<"\t\tyou can use --num-0 to set 123456789 to chars\n";
     cout<<"-o for get output file\n";
     cout<<"-sr for config the start range of password list\n";
     cout<<"-er for config the end of password list\n";
