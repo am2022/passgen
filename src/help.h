7912dba1fdd53d6cb692fdbe2852f5c79deacbf2
am <am2022a2@gmail.com>
2023-03-10 11:18:51 +0330

    update help.h file

--- src/help.h
+++ src/help.h
@@ -14,5 +14,6 @@ void argv_help(){
     cout<<"-imp_char for import a file for setting chars of password\n";
     cout<<"-static_string for add a static string\n";
     cout<<"-e_static_string for add static string end of the passwords\n";
+    cout<<"you can use -showpass and after that you can type y,Y,t,T for see the passwords and other chars for hide them.\n";
     cout<<"you should type -exec end of your inputs";
 }
